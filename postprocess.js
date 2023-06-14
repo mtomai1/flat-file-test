4b80491c43475ae88e80cc88241d3d2a4f48f03d
fk0252 <matteo.tomai@consulenti.allitude.it>
2023-06-14 10:50:15 +0200

    modified:   postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -2,24 +2,30 @@ import { readJson, writeJson} from 'https://deno.land/x/jsonfile/mod.ts'
 
 const filename = Deno.args[0] 
 const json = await readJson(filename)
-console.log(json)
+
 const date_ob = new Date();
+const year = date_ob.getFullYear();
 
-// Step 2: Filter specific data we want to keep and write to a new JSON file
-let year = date_ob.getFullYear();
+const remappedArray = [];
 
-const userName = Object.values(json.user_name); 
-const surnaName = Object.values(json.surnaname); 
-const yearOfWork = year - Object.values(json.recruitment_year); 
+for (const item of data) {
+    const userName = item.user_name; 
+    const surnaName = item.surnaname; 
+    const yearOfWork = year - item.recruitment_year; 
 
-const data = {
-    userName,
-    surnaName,
-    yearOfWork,
-  };
+    const data = {
+        userName,
+        surnaName,
+        yearOfWork,
+      };
+    
+      remappedArray.push(data);
+  }
+
+
 
 
 // Step 3. Write a new JSON file with our filtered data
 const newFilename = `team-core-postprocessed.json` // name of a new file to be saved
-await writeJson(newFilename, data) // create a new JSON file with just the Bitcoin price
+await writeJson(newFilename, remappedArray) // create a new JSON file with just the Bitcoin price
 console.log("Wrote a post process file")
